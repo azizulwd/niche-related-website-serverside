619c517222176b00176ff7c6f968947f50df7268
azizulwd <azizulislamgd@gmail.com>
2021-11-11 01:24:16 +0600

    db connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
 
 
 const app = express();
@@ -12,6 +12,10 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
+// database connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy859.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri, "connected");
 
 app.get('/', (req, res) => {
     res.send('Ready for the Assignment-12');
